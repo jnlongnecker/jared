055901757ab3efd0986010af48e796b26edb59ca
jnlongnecker <jlnecker1@gmail.com>
2022-06-16 01:12:45 -0400

    More styling updates to the text portion of the documents

--- public/components/jwork-demo-content.js
+++ public/components/jwork-demo-content.js
@@ -3,6 +3,25 @@ template.innerHTML = `<link rel="stylesheet" media="screen and (max-width:900px)
 <style>.intro {
     margin-top: 5rem;
     text-align: center;
+}
+
+.content {
+    display: flex;
+    justify-content: center;
+}
+
+.backer {
+    padding: 1rem;
+    background-color: rgba(255,255,255, 0.06);
+    border-radius: 5px;
+    display: inline-block;
+}
+@media screen and (max-width: 900px) {
+    
+    .backer  {
+        padding: 1rem;
+        margin: 1rem;
+    }
 }</style>
 <section>
     <div class="intro">
@@ -24,7 +43,11 @@ template.innerHTML = `<link rel="stylesheet" media="screen and (max-width:900px)
     </div>
 </section>
 
-<slot name="content"></slot>`;
+<div class="content">
+    <div class="backer">
+        <slot name="content"></slot>
+    </div>
+</div>`;
 
 export default class JworkDemocontent extends HTMLElement {
 
